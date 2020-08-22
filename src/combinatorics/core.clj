078d26424c20121e3dd0c10fe4ed6797c97eecc6
gerritjvv <gerritjvv@gmail.com>
2020-08-22 23:34:43 +0200

    add notes and helpers to combinatorics

--- src/combinatorics/core.clj
+++ src/combinatorics/core.clj
@@ -1,10 +1,27 @@
-(ns combinatorics.core)
+(ns 
+  "
+   Choose: (N / K) => choose n k
+  
+   Use when selecting K items out of N
+
+   Multi Choose (( N / K )) => choose n k :rep true :order false
+
+   Use multi choose when:
+     Distributed N things between K options 
+  "
+
+  combinatorics.core)
+
 
 (defn pow [n k]
    "n ^ k"
    (Math/pow (long n) (long k)))
 
 
+;; suprisingly fast up to 100! on modern hardware
+;; (time (double (c/fact 100)))
+;; "Elapsed time: 0.069909 msecs"
+;;  9.332621544394418E157
 (defn fact [n]
    (loop [i (long n) acc (double 1)]
      (if (<= i 1)
@@ -35,3 +52,17 @@
 (defmethod choose {:rep true :order true}
   [n k & _]
   (pow n k))
+
+
+;;;;;;; brute force arlgorithms to help validate
+
+(defn sum-digits [v]
+ (reduce + 0 (map #(- (int %) 48) (str v))))
+
+(defn sum-of-equal [from to equal-to]
+  "return all the numbers who'se digits in range from to add up to equal-to"
+  (filter 
+    #(= (sum-digits %) equal-to)
+     (range from to)))
+
+
